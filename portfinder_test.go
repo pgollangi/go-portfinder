4a4cc03ffbcd04e9d3cf82f06ebba34889f1f1f7
Prasanna Kumar Gollangi <pgollang@opentext.com>
2021-04-14 18:11:04 +0530

    Improve tests

--- portfinder_test.go
+++ portfinder_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestValidateStartPort(t *testing.T) {
+	_, err := GetPort(PortFinderOptions{})
+	if err == nil {
+		t.Fail()
+	}
+	_, err = GetPort(PortFinderOptions{StartPort: -1})
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestValidateStopPort(t *testing.T) {
+	_, err := GetPort(PortFinderOptions{StartPort: 10, StopPort: 9})
+	if err == nil {
+		t.Fail()
+	}
+}
+
 func TestGetPort(t *testing.T) {
 	l, err := net.Listen("tcp", ":3000")
 	defer l.Close()
